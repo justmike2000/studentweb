2f34daf7eed2fd09b392dda76b3fd11064bec6b3
justmike2000 <justmike2000@gmail.com>
2016-07-28 23:23:29 -0400

    Add docstrings

--- studentweb/students/models.py
+++ studentweb/students/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 
 class Student(models.Model):
-    """
+    """ Student Django Model
+        - Represents a student (Sally Salt)
     """
     name = models.CharField(max_length=255)
     email = models.EmailField()
 
     def __unicode__(self):
+        """ Returns a unicode representation of this class"""
         return "%d %s %s" % (self.id, self.name, self.email,)
 
     def my_classes(self):
-        """
-        """
+        """ Returns a list of classes and grades for a student"""
         classes = []
         for semester in Semester.objects.filter(student_class=self.id).all():
              classes.append(semester.to_dict())
         return classes
 
     def to_dict(self):
-        """
-        """
+        """ Returns a dictionary representation of this class"""
         student_dict = {}
         student_dict["studentClasses"] = self.my_classes()
         student_dict["email"] = self.email
@@ -35,33 +35,36 @@ class Student(models.Model):
 
 
 class StudentClass(models.Model):
-    """
+    """ StudentClass Django Model
+        - Identifies a student class (i.e. Math 101) 
     """
     description = models.CharField(max_length=255)
 
     def to_dict(self):
-        """
-        """
+        """ Returns a dictionary representation of this class"""
         return {"%d" % (self.id,): self.description}
 
     def __unicode__(self):
+        """ Returns a unicode representation of this class"""
         return "%d %s" % (self.id, self.description,)
 
 
 class Semester(models.Model):
-    """
+    """ Semester Django Model
+        - Represents a relation between student and class (i.e. Sally Salt with a 2.0 grade) 
     """
     student = models.ForeignKey(Student)
     student_class = models.ForeignKey(StudentClass)
     grade = models.FloatField()
 
     class Meta:
+        """ Meta sublass to specify composite unique fields"""
         unique_together = (('student', 'student_class'),)
 
     def to_dict(self):
-        """
-        """
+        """ Returns a dictionary representation of this class"""
         return {"id": self.student_class.id, "grade": self.grade}
 
-    def __unicode__(self):
+    def __unicode__(self): 
+        """ Returns a unicode representation of this class"""
         return "%d %s %s %d" % (self.id, self.student.name, self.student_class.description, self.grade)
--- studentweb/students/views.py
+++ studentweb/students/views.py
@@ -14,12 +14,25 @@ def index(request):
 
 
 class StudentView(View):
-    """
+    """ StudentView View
+        We only defined a get request method here. 
+        We can expand on this and build a full RESTful view.
     """
 
     def get(self, request):
+        """ Get method for students endpoint
+            - parameters 
+                first: 
+                last:
+                email:
+                average_gpa:
+            - returns: {'students': [<students>], 'classes': [<classes>]}
         """
-        """
+        first = request.GET.get("first", None)
+        last = request.GET.get("last", None)
+        email = request.GET.get("email", None)
+        average_gpa = request.GET.get("average_gpa", None)
+
         data = {"students": [], "classes": []}
 
         for student in Student.objects.all():
